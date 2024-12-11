5dd849bd6d6e6a6880280e48c9c067ccc23c11e8
cempod <cempod@gmail.com>
2024-12-11 09:35:02 +0300

    Help me

---
+++ interface/main_page.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN_PAGE_H
+#define __MAIN_PAGE_H
+
+#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+#include "lvgl.h"
+#else
+#include "lvgl/lvgl.h"
+#endif
+
+void init_main_page(void);
+void load_main_page(void);
+
+#endif
